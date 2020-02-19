0b2b195209d80a05d5858f252d65fe6b92afbce3
Renan Borges <renan.borgesx@gmail.com>
2020-02-19 07:00:38 -0300

    Fim de implementacao [funcionando]

--- semana_5/buscador.py
+++ semana_5/buscador.py
@@ -1,34 +1,34 @@
 ﻿#------------------------------------------------------------------------#
-#           CLASSE BUSCADOR                                              #
+#           FUNCAO ORDENADOR INSERTION SORT                              #
 #           Python 3.8.1 19-02-2020 Renan Borgesx                        #
 #------------------------------------------------------------------------#
 
-class Buscador:
+def insertion_sort(lista):
 
-    def busca_sequencial(self,lista,elemento):
+    index = 0
+    suspenso = 0
 
-        for i in range(len(lista)):
-            if lista[i] == elemento:
-                return i
-                    
-        return False
-
-    '''Melhor algoritmo para busca em uma lista - log 2 n'''
-    def busca_binaria(self,lista,elemento):
-
-        primeiro = 0
-        ultimo = len(lista) - 1
+    for i in range(len(lista) - 1):
         
-        while primeiro <= ultimo:
-            meio = (primeiro + ultimo) // 2
+        menor = False
+        for eq in range(i,-1,-1):      
 
-            if lista[meio] == elemento:
-                return meio
-            else:
-                if elemento < lista[meio]:
-                    ultimo = meio -1
-                else:
-                    primeiro = meio + 1
+            if lista[i+1] < lista[eq]:
+                index = eq
+                suspenso = lista[i+1]
+                menor = True
 
-        return -1
+        if menor:
+            for cont in range(i+1,index,-1):
+                lista[cont] = lista[cont -1]
+
+                if lista[cont-1] > suspenso:    
+                    lista[cont - 1] = suspenso
+        print(lista)
+                    
+                    
+    return lista
+            
+                                    
+    
     
