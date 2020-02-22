cbc61d9850543646b85c2ac6a0a502c68338da7f
Renan Borges <renan.borgesx@gmail.com>
2020-02-22 17:04:37 -0300

    Envio de algoritmo do elefante

---
+++ semana_6/EXEC_elefantesRecursivo.py
@@ -0,0 +1,46 @@
+﻿#------------------------------------------------------------------------#
+#           ELEFANTES INCOMODAM RECURSIVO                                #
+#           Python 3.8.1 22-02-2020 Renan Borgesx                        #
+#------------------------------------------------------------------------#
+
+def incomodam(n):
+    n -= 1
+    string = ""
+    
+    if n < 0:
+        return string
+    else:        
+        string = "incomodam "
+        return string + incomodam(n)
+    
+
+def elefantes(n):
+
+    string = ""
+        
+    if n < 1:
+        return string        
+    else:
+        if n >= 1 :
+            if n == 1:
+                string = "Um elefante incomoda muita gente. "
+                print(""+elefantes(n-1) + string,"\n")
+            else:
+                                
+                string = "elefantes "+incomodam(n)+"muito mais. "
+                print(str(n)+" "+ elefantes(n-1) + string,"\n")
+                
+                string = "elefantes incomodam muita gente. "
+                print(str(n)+" "+ string,"\n")
+   
+         
+    
+
+    return ""
+        
+
+             
+        
+
+
+
---
+++ semana_6/EXEC_elefantesRecursivo_s6.py
@@ -0,0 +1,53 @@
+﻿#------------------------------------------------------------------------#
+#           ELEFANTES INCOMODAM RECURSIVO                                #
+#           Python 3.8.1 22-02-2020 Renan Borgesx                        #
+#------------------------------------------------------------------------#
+import textwrap
+
+def incomodam(n):
+    n -= 1
+    string = ""
+    
+    if n < 0:
+        return string
+    else:        
+        string = "incomodam "
+        return string + incomodam(n)
+    
+
+def elefantes(n):
+
+    verso1 = ""
+    verso2 = ""
+    verso3 = ""
+    musica = ""
+        
+    if n < 0:
+        return musica        
+    else:
+        verso1 = "Um elefante incomoda muita gente\n"
+        verso2 = "elefantes incomodam muita gente\n"        
+        verso3 = "elefantes "+incomodam(n)+"muito mais\n"
+        
+        if n == 1:
+            musica = elefantes(n-1) + musica + verso1
+            
+        elif n > 0:
+            musica = musica + str(n)+" "+ verso3
+            musica = elefantes(n-1) + musica + str(n)+" "+ verso2
+
+            
+                         
+
+
+    return musica
+   
+         
+    
+        
+
+             
+        
+
+
+
